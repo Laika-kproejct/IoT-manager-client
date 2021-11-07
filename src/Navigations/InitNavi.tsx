ce1e9cbf8d7ae4dec255fcc97856d573bee81dc6
dhkwak7791 <kkddhh1998@gmail.com>
2021-11-08 01:36:43 +0900

    총 네비게이션

---
+++ src/Navigations/InitNavi.tsx
@@ -0,0 +1,41 @@
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import React from 'react';
+import { Text } from 'react-native';
+import { Icon } from 'react-native-vector-icons/Icon';
+import styled from 'styled-components';
+import { createStackNavigator } from '@react-navigation/stack';
+import MainScreen from '../Screen/MainScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import LoginScreen from '../Screen/LoginScreen';
+import SignUpScreen from '../Screen/SignUpScreen';
+import MainBottomTab from './MainBottomTab';
+
+const Stack = createStackNavigator();
+
+const InitNavi = () => {
+    return(
+    <Stack.Navigator initialRouteName="Main">
+    <Stack.Screen
+    name="Main"
+    component={MainScreen}
+    options={{headerShown:false}}/>
+
+    <Stack.Screen
+    name="Login"
+    component={LoginScreen}
+    options={{headerShown:false}}/>
+
+    <Stack.Screen
+    name="SignUp"
+    component={SignUpScreen}
+    options={{headerShown:false}}/>
+    
+    <Stack.Screen
+    name="Bottom"
+    component={MainBottomTab}
+    options={{headerShown:false}}/>
+    </Stack.Navigator>
+        
+    );
+};
+export default InitNavi;
