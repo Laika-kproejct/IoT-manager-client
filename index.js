17ec2a5c1a2cba86ad584874e76b570b6e8f8c85
dhkwak7791 <kkddhh1998@gmail.com>
2021-12-15 17:30:30 +0900

    FCM title body 끝

--- index.js
+++ index.js
@@ -7,5 +7,14 @@ import App from './App';
 import {name as appName} from './app.json';
 import messaging from '@react-native-firebase/messaging';
 
-
+messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+        console.log('Message handled in the background!', remoteMessage);
+        //  여기에 로직을 작성한다.
+        //  remoteMessage.data로 메세지에 접근가능
+        //  remoteMessage.from 으로 topic name 또는 message identifier
+        //  remoteMessage.messageId 는 메시지 고유값 id
+        //  remoteMessage.notification 메시지와 함께 보내진 추가 데이터
+        //  remoteMessage.sentTime 보낸시간
+        });
+        
 AppRegistry.registerComponent(appName, () => App);
--- src/Screen/MainScreen.tsx
+++ src/Screen/MainScreen.tsx
@@ -36,24 +36,16 @@ const Touch =Styled.TouchableOpacity`
 const Label = Styled.Text``;
 
 const MainScreen = ({navigation}:{navigation:any}) => {
-    messaging().setBackgroundMessageHandler(async (remoteMessage) => {
-        console.log('Message handled in the background!', remoteMessage);
-        //  여기에 로직을 작성한다.
-        //  remoteMessage.data로 메세지에 접근가능
-        //  remoteMessage.from 으로 topic name 또는 message identifier
-        //  remoteMessage.messageId 는 메시지 고유값 id
-        //  remoteMessage.notification 메시지와 함께 보내진 추가 데이터
-        //  remoteMessage.sentTime 보낸시간
-        });
-        // Foreground 상태인 경우
+    
+
+    // Foreground 상태인 경우
         React.useEffect(() => {
-        const unsubscribe = messaging().onMessage(async remoteMessage => {
-            Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+        const unsubscribe = messaging().onMessage(async (remoteMessage: any) => {
+            Alert.alert(remoteMessage.notification.title, remoteMessage.notification.body);
+            
         });
         return unsubscribe;
         });
-
-    
     
     /*
     async function requestUserPermission() {
